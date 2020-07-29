9632aa88554a8521a3e8ca55fe51d5466b26f22c
Abhishek Patel <abhishekpatel@pop-os.localdomain>
2020-07-29 22:54:25 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <v-app>
     <v-app-bar color="primary" app dark dense>
-      <v-toolbar-title>Sitaram Enterprise</v-toolbar-title>
+      <v-toolbar-title @click="$router.push('/')"
+        >Sitaram Enterprise</v-toolbar-title
+      >
       <v-spacer></v-spacer>
       <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
     </v-app-bar>
@@ -9,19 +11,19 @@
     <v-navigation-drawer v-model="drawer" absolute temporary>
       <v-list class="mt-16" nav dense>
         <v-list-item-group active-class="deep-purple--text text--accent-4">
-          <v-list-item
+          <router-link
+            :to="`${item.link}`"
             v-for="(item, id) in items"
             :key="id"
-            @click="$router.push({ path: item.link })"
           >
-            <v-list-item-icon>
-              <v-icon> {{ item.icon }} </v-icon>
-            </v-list-item-icon>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon> {{ item.icon }} </v-icon>
+              </v-list-item-icon>
 
-            <v-list-item-title>
-              <router-link :to="`${item.link}`"> {{ item.title }} </router-link>
-            </v-list-item-title>
-          </v-list-item>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </router-link>
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
